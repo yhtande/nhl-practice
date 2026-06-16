d1b1c9c9b3b644a6d074378f47b1b907dad9ffd3
yhtande <yanhan.t@gmail.com>
2026-06-16 15:51:01 +0800

    chore: sync mart_shot_zones.sql to current production version (shot_zone classification)

--- nhl_dbt/models/gold/mart_shot_zones.sql
+++ nhl_dbt/models/gold/mart_shot_zones.sql
@@ -1,27 +1,36 @@
 SELECT
     ts.team_id,
     g.season_year,
-    
+    CASE
+        WHEN fp.coord_x >= 69 AND fp.coord_y BETWEEN -22 AND 22  THEN 'slot'
+        WHEN fp.coord_x >= 25 AND fp.coord_y > 22                THEN 'left_wing'
+        WHEN fp.coord_x >= 25 AND fp.coord_y < -22               THEN 'right_wing'
+        WHEN fp.coord_x BETWEEN 25 AND 54                        THEN 'point'
+        ELSE 'other'
+    END                                                           AS shot_zone,
+
     -- Shot volume
     COUNT(*) FILTER (WHERE fp.event IN ('Shot', 'Missed Shot', 'Blocked Shot', 'Goal'))  AS total_shot_attempts,
-    COUNT(*) FILTER (WHERE fp.event = 'Shot')                                             AS shots_on_goal,
-    COUNT(*) FILTER (WHERE fp.event = 'Goal')                                             AS total_goals,
-    
+    COUNT(*) FILTER (WHERE fp.event = 'Shot')                                            AS shots_on_goal,
+    COUNT(*) FILTER (WHERE fp.event = 'Goal')                                            AS total_goals,
+
     -- Shot efficiency
-    ROUND(COUNT(*) FILTER (WHERE fp.event = 'Goal') * 100.0 / 
-          NULLIF(COUNT(*) FILTER (WHERE fp.event = 'Shot'), 0), 2)                        AS shot_conversion_rate,
-    
+    ROUND(COUNT(*) FILTER (WHERE fp.event = 'Goal') * 100.0 /
+          NULLIF(COUNT(*) FILTER (WHERE fp.event = 'Shot'), 0), 2)                       AS shot_conversion_rate,
+
     -- Coordinate coverage
-    COUNT(*) FILTER (WHERE fp.event = 'Goal' AND fp.coord_x IS NOT NULL)                  AS goals_with_coords,
-    COUNT(*) FILTER (WHERE fp.event = 'Goal' AND fp.coord_x IS NULL)                      AS goals_missing_coords,
+    COUNT(*) FILTER (WHERE fp.event = 'Goal' AND fp.coord_x IS NOT NULL)                 AS goals_with_coords,
+    COUNT(*) FILTER (WHERE fp.event = 'Goal' AND fp.coord_x IS NULL)                     AS goals_missing_coords,
     ROUND(COUNT(*) FILTER (WHERE fp.event = 'Goal' AND fp.coord_x IS NULL) * 100.0 /
-          NULLIF(COUNT(*) FILTER (WHERE fp.event = 'Goal'), 0), 2)                        AS goals_missing_coords_pct
+          NULLIF(COUNT(*) FILTER (WHERE fp.event = 'Goal'), 0), 2)                       AS goals_missing_coords_pct
 
 FROM {{ ref('fct_play') }} fp
 LEFT JOIN {{ ref('stg_game') }} g
     ON fp.game_id = g.game_id
 LEFT JOIN {{ ref('stg_game_teams_stats') }} ts
-    ON fp.game_id = ts.game_id 
+    ON fp.game_id = ts.game_id
     AND fp.team_id_for = ts.team_id
-WHERE ts.team_id IS NOT NULL AND g.season_year >= 2010
-GROUP BY ts.team_id, g.season_year
+WHERE ts.team_id IS NOT NULL
+AND g.season_year >= 2010
+GROUP BY ts.team_id, g.season_year, shot_zone
+ORDER BY shot_conversion_rate DESC
